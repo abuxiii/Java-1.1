ab0c63ac0bb08a44c24251ed22c1e5653b8fe66c
Abubakr Inogomov <abubbaakr13@gmail.com>
2024-08-05 17:02:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var usersName = prompt('Write down your name')
+console.log('My name is ' + usersName );
+
+var X = +prompt('Write your first number' )
+console.log('Z = ' + X)
+
+
+var Y = +prompt('Write your second number' )
+console.log('Y = ' + Y)
+
+var Z = +prompt('Write your third number' )
+console.log('Z = ' + Z)
+
+var res = alert((X + + Y + + Z)/3)
+console.log( (X + + Y + + Z)/3 );
+
+
